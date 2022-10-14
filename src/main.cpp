889c78274fdbddeaf238036ca647a948e8d725c4
Kostya <parashkasergeev@gmail.com>
2022-10-14 12:41:08 +0300

    Added shader to my game!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
 #include<glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "Renderer/ShaderProgram.h"
+GLfloat point[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+GLfloat colors[] = {
+    1.0f,0.0f,0.0f,
+    0.0f,1.0f,0.0f,
+    0.0f,0.0f,1.0f
+};
+
+const char * vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main(){"
+"    color  = vertex_color;"
+"    gl_Position = vec4(vertex_position,1.0);"
+"}";
+//vec4 var = vec4(vec2,1.0,0.0);
+const char * fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main(){"
+"    frag_color  = vec4(color,1.0);"
+"}";
+
 
 int gWindowSizeX = 640;
 int gWindowSizeY = 480;
@@ -45,11 +76,44 @@ int main(void)
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
 
     glClearColor(1, 0, 1, 1);
+    
+    std::string vertexShader(vertex_shader);
+    std::string fragmentShader(fragment_shader);
+    Renderer::ShaderProgram shaderProgram(vertexShader, fragmentShader);
+
+    GLuint points_vbo = 0;
+    glGenBuffers(1,&points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER,points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1,&vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
     while (!glfwWindowShouldClose(pWindow))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        shaderProgram.use();
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES,0,3);
+
         glfwSwapBuffers(pWindow);
+
+
         glfwPollEvents();
     }
 
